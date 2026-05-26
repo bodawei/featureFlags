89d42d70db838612c2d771930a65d42b449a02eb
播大衛 <OceansOfRice@gmail.com>
2026-05-26 11:27:58 -0700

    argh

--- src/main/java/com/featureflags/FeatureFlagsApplication.java
+++ src/main/java/com/featureflags/FeatureFlagsApplication.java
@@ -3,7 +3,6 @@ package com.featureflags;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class FeatureFlagsApplication {
 
     public static void main(String[] args) {
