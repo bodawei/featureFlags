556420c0780fb924ae8cba9dd8b80eb163d8b512
播大衛 <OceansOfRice@gmail.com>
2026-05-26 11:22:00 -0700

    remove a dumb comment

--- src/main/java/com/featureflags/FeatureFlagsApplication.java
+++ src/main/java/com/featureflags/FeatureFlagsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeatureFlagsApplication {
 
-  // foo
     public static void main(String[] args) {
         SpringApplication.run(FeatureFlagsApplication.class, args);
     }
