02bb9b586ffa38ee5cc48c3d87940d5080256699
柏大衛 <OceansOfRice@gmail.com>
2026-06-24 11:24:50 -0700

    add formatting'

--- src/main/java/com/featureflags/FeatureFlagsApplication.java
+++ src/main/java/com/featureflags/FeatureFlagsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeatureFlagsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FeatureFlagsApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(FeatureFlagsApplication.class, args);
+  }
 }
